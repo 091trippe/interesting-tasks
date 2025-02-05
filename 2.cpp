e9b74b254b7c0e3215c42e52dd5fcc78cbe02e10
091trippe <m1nx0r333@gmail.com>
2025-02-05 13:03:09 +0200

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Введіть кількість рядків: ";
+    cin >> n;
+
+    vector<vector<int>> pascal(n);
+
+    for (int i = 0; i < n; i++) {
+        pascal[i].resize(i + 1, 1); 
+
+        
+        for (int j = 1; j < i; j++) {
+           
+            pascal[i][j] = pascal[i - 1][j - 1] + pascal[i - 1][j];
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int k = 0; k < n - i - 1; k++) {
+            cout << "  ";
+        }
+        for (int j = 0; j < pascal[i].size(); j++) {
+            cout << pascal[i][j] << "   ";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
