552604014187e2f34551d2c3db58178e1e424cea
091trippe <m1nx0r333@gmail.com>
2025-02-05 13:17:47 +0200

    Add files via upload

---
+++ 3.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void sieve_of_eratosthenes(int n, vector<bool>& is_prime) {
+    is_prime.assign(n + 1, true);
+    is_prime[0] = is_prime[1] = false;
+
+    for (int i = 2; i * i <= n; ++i) {
+        if (is_prime[i]) {
+            for (int j = i * i; j <= n; j += i) {
+                is_prime[j] = false;
+            }
+        }
+    }
+}
+
+void factorize(int num) {
+    vector<bool> is_prime;
+    sieve_of_eratosthenes(num, is_prime);
+    vector<int> factors;
+
+    for (int i = 2; i <= num; ++i) {
+        while (is_prime[i] && num % i == 0) {
+            factors.push_back(i);
+            num /= i;
+        }
+    }
+
+    cout << "Розклад числа: ";
+    for (size_t i = 0; i < factors.size(); ++i) {
+        if (i > 0) cout << ", ";
+        cout << factors[i];
+    }
+    cout << endl;
+}
+
+int main() {
+    int n;
+    cout << "Введіть число для розкладу на прості множники: ";
+    cin >> n;
+    factorize(n);
+    return 0;
+}
---
+++ 4.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <cmath>
+#include <string>
+using namespace std;
+
+
+bool isPalindrome(int num) {
+    string str = to_string(num); 
+    int start = 0;
+    int end = str.length() - 1;
+
+   
+    while (start < end) {
+        if (str[start] != str[end]) {
+            return false; 
+        }
+        start++;
+        end--;
+    }
+    return true; 
+}
+
+int main() {
+    
+    for (int i = 1; i <= 99; i++) {
+        if (isPalindrome(i)) { 
+            int square = i * i; 
+            if (isPalindrome(square)) { 
+                cout << i << " " << square << endl; 
+            }
+        }
+    }
+    return 0;
+}
