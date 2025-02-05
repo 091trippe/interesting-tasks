579abddb88f4012b895c4817ed94425db368c1ac
TRIPPE <TRIPPE@KOMPUTER>
2025-02-05 12:59:42 +0200

    Добавьте файлы проекта.

---
+++ 1/1.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void findTriples(int n) {
+    for (int a = 1; a <= n; a++) {
+        for (int b = 1; b <= n; b++) {
+            int c = a * a + b * b; 
+            if (c <= n) {
+                cout << "(" << a << ", " << b << ", " << c << ")\n";
+            }
+        }
+    }
+}
+
+int main() {
+    int n;
+    cout << "Введіть n: ";
+    cin >> n;
+
+    if (n <= 0) {
+        cout << "n має бути більше 0!\n";
+        return 1;
+    }
+
+    cout << "Трійки чисел (a, b, c), що задовольняють рівнянню:\n";
+    findTriples(n);
+
+    return 0;
+}
