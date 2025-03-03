fbb4bd9cb941bcfe7d380d5bf068c25b95f7a611
091trippe <m1nx0r333@gmail.com>
2025-03-03 13:20:44 +0200

    PS : 3.cpp

--- 3.cpp
+++ 3.cpp
@@ -1,45 +1,25 @@
-﻿#include <iostream>
+#include <iostream>
 #include <vector>
-
 using namespace std;
 
-void sieve_of_eratosthenes(int n, vector<bool>& is_prime) {
-    is_prime.assign(n + 1, true);
-    is_prime[0] = is_prime[1] = false;
+int main() {
+    int n = 120; 
+    vector<bool> omni (n + 1, true); 
+    omni[0] = omni[1] = false; 
 
-    for (int i = 2; i * i <= n; ++i) {
-        if (is_prime[i]) {
-            for (int j = i * i; j <= n; j += i) {
-                is_prime[j] = false;
+    for (int p = 2; p * p <= n; ++p) { 
+        if (omni[p]) { 
+            for (int multiple = p * p; multiple <= n; multiple += p) { 
+                omni[multiple] = false;
             }
         }
     }
-}
 
-void factorize(int num) {
-    vector<bool> is_prime;
-    sieve_of_eratosthenes(num, is_prime);
-    vector<int> factors;
-
-    for (int i = 2; i <= num; ++i) {
-        while (is_prime[i] && num % i == 0) {
-            factors.push_back(i);
-            num /= i;
+    for (int i = 2; i <= n; ++i) {
+        if (omni[i]) {
+          cout << i << " ";
         }
     }
-
-    cout << "Розклад числа: ";
-    for (size_t i = 0; i < factors.size(); ++i) {
-        if (i > 0) cout << ", ";
-        cout << factors[i];
-    }
-    cout << endl;
-}
-
-int main() {
-    int n;
-    cout << "Введіть число для розкладу на прості множники: ";
-    cin >> n;
-    factorize(n);
+   cout << std::endl;
     return 0;
 }
