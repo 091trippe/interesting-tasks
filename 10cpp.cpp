c369c4db75dd7a6a34ebb6caa82e855aa9d4d301
091trippe <m1nx0r333@gmail.com>
2025-03-20 13:51:50 +0200

    Update 10cpp.cpp

--- 10cpp.cpp
+++ 10cpp.cpp
@@ -1,39 +1,32 @@
-﻿#include <iostream>
+#include <iostream>
 #include <vector>
 
 using namespace std;
 
-void findMagicVectors(int N, int index, int sum, int product, int last, vector<int>& current) {
-    if (index == N) {
-        if (sum == product) {
-            for (int num : current) {
-                cout << num << " ";
-            }
+void findMagicVectors(vector<int> &vec, int N, int sum, int product, int last) {
+    if (vec.size() == N) {
+        if (sum == product) { 
+            for (int num : vec) cout << num << " ";
             cout << endl;
         }
         return;
     }
 
-    for (int i = last; i <= sum + 1; ++i) {  
-        current.push_back(i);
-        findMagicVectors(N, index + 1, sum + i, product * i, i, current);
-        current.pop_back();
+    for (int i = last; i <= sum + 1; i++) {
+        vec.push_back(i);
+        findMagicVectors(vec, N, sum + i, product * i, i);
+        vec.pop_back();
     }
 }
 
 int main() {
     int N;
-    cout << "Введіть значення N: ";
+    cout << "Введіть N: ";
     cin >> N;
 
-    if (N <= 0) {
-        cout << "N має бути більше 0." << endl;
-        return 1;
-    }
-
-    vector<int> current;
-    cout << "Чарівні вектори для N = " << N << ":" << endl;
-    findMagicVectors(N, 0, 0, 1, 1, current);
+    vector<int> vec;
+    cout << "Чарівні вектори:\n";
+    findMagicVectors(vec, N, 0, 1, 1);
 
     return 0;
 }
