cdc3c2d2665f6911485520be80af16e555ceed31
091trippe <m1nx0r333@gmail.com>
2025-03-20 13:47:09 +0200

    Update 7cpp.cpp

--- 7cpp.cpp
+++ 7cpp.cpp
@@ -1,44 +1,33 @@
-﻿#include <iostream>
+#include <iostream>
 #include <vector>
-#include <cmath>
-
 using namespace std;
 
 bool isPrime(int num) {
-    if (num <= 1) return false;
-    if (num == 2) return true;
-    if (num % 2 == 0) return false;
-    int sqrtNum = sqrt(num);
-    for (int i = 3; i <= sqrtNum; i += 2) {
+    if (num < 2) return false;
+    for (int i = 2; i * i <= num; i++) {
         if (num % i == 0) return false;
     }
     return true;
 }
 
+void findTwinPrimes(int n) {
+    for (int i = n; i <= 2 * n - 2; i++) {
+        if (isPrime(i) && isPrime(i + 2)) {
+            cout << i << " " << i + 2 << endl;
+        }
+    }
+}
+
 int main() {
     int n;
-    cout << "Введіть значення n: ";
+    cout << "Введіть число n: ";
     cin >> n;
-
-    vector<int> primes;
-    for (int i = n; i <= 2 * n; ++i) {
-        if (isPrime(i)) {
-            primes.push_back(i);
-        }
+    
+    if (n < 1) {
+        cout << "Число повинно бути більше 0" << endl;
+    } else {
+        findTwinPrimes(n);
     }
-
-    cout << "Пари простих чисел-близнюків у діапазоні від " << n << " до " << 2 * n << ":\n";
-    bool found = false;
-    for (size_t i = 0; i < primes.size() - 1; ++i) {
-        if (primes[i + 1] - primes[i] == 2) {
-            cout << "(" << primes[i] << ", " << primes[i + 1] << ")\n";
-            found = true;
-        }
-    }
-
-    if (!found) {
-        cout << "Пар простих чисел-близнюків не знайдено.\n";
-    }
-
+    
     return 0;
 }
